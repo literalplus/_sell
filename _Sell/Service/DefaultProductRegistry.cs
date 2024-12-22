77fc19227aac397433825920e46438e4dcff1fec
Philipp Nowak <git@lit.plus>
2024-12-22 13:23:37 +0100

    Update example prices

--- _Sell/Service/DefaultProductRegistry.cs
+++ _Sell/Service/DefaultProductRegistry.cs
@@ -5,27 +5,27 @@ namespace _Sell.Service
 {
     public class DefaultProductRegistry : IProductRegistry
     {
+        private readonly Dictionary<int, Product> _productsDictionary = new Dictionary<int, Product>();
+
         private static readonly List<Product> DefaultProducts = new List<Product>()
         {
-            new Product(1, "Gulaschsuppe", new Price(3, 80)),
-            new Product(2, "Heißer Löwe", new Price(3, 50)),
-            new Product(3, "Kinderpunsch", new Price(2, 50)),
-            new Product(4, "Löwe w/ Brüll", new Price(4, 50)),
-            new Product(5, "Aufstrichbrot", new Price(1, 50)),
-            new Product(6, "Softdrinks 0,5l", new Price(1, 50)),
-            new Product(7, "Speckstangerl", new Price(2, 50)),
-            new Product(8, "Autofahrerpunsch", new Price(3, 0), "Autofahrer-\npunsch"),
-            new Product(9, "Glühwein", new Price(3, 50)),
-            new Product(10, "Eierlikör", new Price(2, 50)),
-            new Product(11, "Prosecco", new Price(3, 0)),
-            new Product(12, "Tee", new Price(1, 50)),
-            new Product(13, "Bier", new Price(2, 50)),
-            new Product(14, "1/8 Wein", new Price(2, 20)),
-            new Product(15, "Stamperl Schnaps", new Price(2, 50), "Stamperl\nSchnaps"),
-            new Product(16, "Weißer Spritzer", new Price(2, 50), "Weißer\nSpritzer")
+            new Product(1, "Heißer Löwe", new Price(4, 0)),
+            new Product(2, "Kinderpunsch", new Price(3, 0)),
+            new Product(3, "Autofahrerpunsch", new Price(3, 50), "Autofahrer-\npunsch"),
+            new Product(4, "Löwe w/ Brüll", new Price(5, 0)),
+            new Product(5, "Glühwein", new Price(3, 50)),
+            
+            new Product(10, "Softdrinks 0,5l", new Price(2, 50)),
+            new Product(11, "Wasser Fl.", new Price(2, 0)),
+            new Product(12, "Bier 1/2", new Price(3, 0)),
+            new Product(13, "Bier 1/3", new Price(2, 0)),
+            
+            new Product(20, "Prosecco", new Price(4, 0)),
+            new Product(21, "Spritzer 1/4", new Price(2, 0)),
+            new Product(22, "Stamperl Schnaps", new Price(2, 0), "Stamperl\nSchnaps"),
+            new Product(23, "Tee", new Price(1, 50)),
+            //new Product(18, "Red Bull", new Price(3, 0))
         };
-
-        private readonly Dictionary<int, Product> _productsDictionary = new Dictionary<int, Product>();
 
         public DefaultProductRegistry()
         {
